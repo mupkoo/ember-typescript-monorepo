de0951c9bf270ffc690988c0707f694e2d35967e
Mirko Akov <mirko.akov@icloud.com>
2023-02-21 16:39:26 +0200

    Create the projects

---
+++ apps/one/tests/test-helper.js
@@ -0,0 +1,12 @@
+import Application from 'one/app';
+import config from 'one/config/environment';
+import * as QUnit from 'qunit';
+import { setApplication } from '@ember/test-helpers';
+import { setup } from 'qunit-dom';
+import { start } from 'ember-qunit';
+
+setApplication(Application.create(config.APP));
+
+setup(QUnit.assert);
+
+start();
